df77af1ce4ab8e5ac174895e634eab2d762564e6
Rupeshaba <76235309+Rupeshaba@users.noreply.github.com>
2026-07-10 14:11:19 +0530

    Create branding.js

---
+++ src/constants/branding.js
@@ -0,0 +1,12 @@
+// Default thumbnails shown when a course/book/etc. has no uploaded image,
+// and the app-wide logo (also used for the favicon). Keeping these in one
+// place means updating a brand asset later is a one-line change.
+
+export const DEFAULT_THUMBNAILS = {
+  freeCourses: 'https://res.cloudinary.com/dhniudiwg/image/upload/v1783496658/ChatGPT_Image_Jul_8_2026_01_04_02_PM_nrak15.png',
+  books: 'https://res.cloudinary.com/dhniudiwg/image/upload/v1783496658/ChatGPT_Image_Jul_8_2026_01_09_49_PM_bx3jst.png',
+  store: 'https://res.cloudinary.com/dhniudiwg/image/upload/v1783496658/ChatGPT_Image_Jul_8_2026_01_02_48_PM_o8b7z5.png',
+  progress: 'https://res.cloudinary.com/dhniudiwg/image/upload/v1783496658/ChatGPT_Image_Jul_8_2026_01_11_37_PM_hz5kvh.png',
+}
+
+export const APP_LOGO_URL = 'https://res.cloudinary.com/dhniudiwg/image/upload/v1783575514/logo_1_nnrpqg.png'
