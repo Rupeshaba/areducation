caaa143f729c60a0327080fec808ed51e90c0569
Rupeshaba <76235309+Rupeshaba@users.noreply.github.com>
2026-07-27 09:47:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,25 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // ya "0.0.0.0"
     port: 5173,
+    allowedHosts: [
+      'sb-3ur4rztd7lww.vercel.run',
+    ],
     proxy: {
-      '/api': { target: 'https://areducation-backend.onrender.com', changeOrigin: true },
-      '/uploads': { target: 'https://areducation-backend.onrender.com', changeOrigin: true },
-      '/socket.io': { target: 'https://areducation-backend.onrender.com', changeOrigin: true, ws: true },
+      '/api': {
+        target: 'https://areducation-backend.onrender.com',
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'https://areducation-backend.onrender.com',
+        changeOrigin: true,
+      },
+      '/socket.io': {
+        target: 'https://areducation-backend.onrender.com',
+        changeOrigin: true,
+        ws: true,
+      },
     },
   },
 })
